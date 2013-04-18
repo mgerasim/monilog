cd37b9cdb24fcb1a5f9119c4aa892fcef86d8fd1
Mikahil Gerasimov <GerasimovMN@khv.dv.rt.ru>
2013-04-18 13:49:38 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monilog::Application.config.secret_token = 'e12378531af3514a04657882e2accd92ec64d50b7e4a113dc4cf2c3dfe9a2aca8ee64386cbdeb2f66045d6ed35888c28800dbb487f4c335f466d79ece856dc6d'
