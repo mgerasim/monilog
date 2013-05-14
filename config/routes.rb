9bf14c3c118cb398b3b96d7eb23fbad0d8e5bf03
Mikahil Gerasimov <GerasimovMN@khv.dv.rt.ru>
2013-05-15 08:14:45 +1100

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,19 @@ Monilog::Application.routes.draw do
   root :to => "home#index"
   devise_for :users
   resources :users
+  
+  resources :values
+  
+  resources :counters
+
+  resources :counters do
+    resources :values
+  end
+
+  resources :sites do
+    resources :counters do
+	resources :value
+    end
+  end
+
 end
