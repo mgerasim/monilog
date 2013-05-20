bc68a22e1bf099290814556dfbe5163be7394015
Mikahil Gerasimov <GerasimovMN@khv.dv.rt.ru>
2013-05-20 17:07:15 +1100

    sh

---
+++ monilog.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget -O /tmp/tmp.html http://monilog.herokuapp.com/counters/1/values/new
+wget -O /tmp/tmp.html http://monilog.herokuapp.com/counters/4/values/new
+wget -O /tmp/tmp.html http://monilog.herokuapp.com/counters/3/values/new
+wget -O /tmp/tmp.html http://monilog.herokuapp.com/counters/2/values/new
